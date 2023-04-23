cd0804d53867d270f18b4bc2a10818d8fbac1d3b
jakobkirton <jakobkirton@gmail.com>
2023-04-23 16:52:40 +0100

    Updated Example to use DecodeID() and MachineIDBitLength, SequenceBitLength through its Settings.

--- SnowflakeGenerator.Example/Program.cs
+++ SnowflakeGenerator.Example/Program.cs
@@ -1,11 +1,12 @@
 ﻿using SnowflakeGenerator;
 using SnowflakeGenerator.Exceptions;
-using System.Xml;
 
 var settings = new Settings
 {
     MachineID = 0,
-    CustomEpoch = new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.Zero)
+    CustomEpoch = new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.Zero),
+    MachineIDBitLength = 16,
+    SequenceBitLength = 6
 };
 Snowflake sonyflake = new(settings);
 
@@ -14,9 +15,13 @@ Console.WriteLine("Generating 10 unique ID values.\n");
 for (int i = 0; i < 10; i++)
 {
     long uniqueId;
+    long timeStamp;
+    long machineId;
+    long sequence;
     try
     {
         uniqueId = sonyflake.NextID();
+        (timeStamp, machineId, sequence) = sonyflake.DecodeID(uniqueId);
     }
     catch (SnowflakeException ex)
     {
@@ -25,6 +30,15 @@ for (int i = 0; i < 10; i++)
     }
 
     Console.WriteLine($"Generated ID: {uniqueId}");
+
+    DateTimeOffset unixEpoch = new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero);
+
+    long unixTimestamp = timeStamp + settings.CustomEpoch?.ToUnixTimeMilliseconds() ?? 0;
+    DateTimeOffset generatedTime = unixEpoch.AddMilliseconds(unixTimestamp);
+
+    Console.WriteLine($"Generated TimeStamp: {generatedTime:yyyy-MM-ddTHH:mm:ss.fffZ}");
+    Console.WriteLine($"Generated MachineID: {machineId}");
+    Console.WriteLine($"Generated Sequence: {sequence}");
 }
 
 Console.WriteLine("\nEnter any key to Exit.");
