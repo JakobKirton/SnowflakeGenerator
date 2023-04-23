9e2d12bbb58096d5ce71c920f57209a3b7f4d8b9
jakobkirton <jakobkirton@gmail.com>
2023-04-23 11:52:29 +0100

    Added XML documentation to Exceptions

--- SnowflakeGenerator/Exceptions/InvalidCustomEpochException.cs
+++ SnowflakeGenerator/Exceptions/InvalidCustomEpochException.cs
@@ -2,6 +2,10 @@
 {
     public class InvalidCustomEpochException : SnowflakeException
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidCustomEpochException"/> class with a specified error message.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
         public InvalidCustomEpochException(string message) : base(message) { }
     }
 }
--- SnowflakeGenerator/Exceptions/InvalidMachineIdException.cs
+++ SnowflakeGenerator/Exceptions/InvalidMachineIdException.cs
@@ -2,6 +2,10 @@
 {
     public class InvalidMachineIdException : SnowflakeException
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidMachineIdException"/> class with a specified error message.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
         public InvalidMachineIdException(string message) : base(message) { }
     }
 }
